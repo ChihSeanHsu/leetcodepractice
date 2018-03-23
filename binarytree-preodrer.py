db4e83943f05b58413b9a0fbe36f290bf43135ca
Vincentsean <34153546+Vincentsean@users.noreply.github.com>
2018-03-23 10:29:37 +0800

    Create binarytree-preodrer.py

---
+++ binarytree-preodrer.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result, stack = [], [(root, False)]
+        while stack:
+            root, is_visit = stack.pop()
+            if root is None:
+                continue
+            elif is_visit:
+                result.append(root.val)
+            else:
+                stack.append((root.right, False))
+                stack.append((root.left, False))
+                stack.append((root, True))
+                
+        return result
