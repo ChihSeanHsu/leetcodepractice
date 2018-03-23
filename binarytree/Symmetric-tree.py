d7f38c6c8782b5047aff1c86298f59f212d752b2
Vincentsean <34153546+Vincentsean@users.noreply.github.com>
2018-03-23 16:29:22 +0800

    Update Symmetric-tree.py

--- binarytree/Symmetric-tree.py
+++ binarytree/Symmetric-tree.py
@@ -8,7 +8,6 @@
 class Solution:
     def go(self, root, layer, stack):
         if root:
-            
             try:
                 stack[layer].append(root.val)
             except:
@@ -23,10 +22,7 @@ class Solution:
             except:
                 stack.append([])
                 stack[layer].append('e')
-        
-        return stack
-        
-        
+        return stack  
         
     def isSymmetric(self, root):
         """
@@ -43,5 +39,30 @@ class Solution:
                 pass
             else:
                 return False
-            
         return True
+    
+    
+    
+class Solution2:
+    def go(self, right, left):
+        if (right==None) and (left!=None):
+            return False
+        elif (right!=None) and (left==None):
+            return False
+        elif (right==None) and (left==None):
+            return True
+        elif right.val != left.val:
+            return False
+        else:
+            return (self.go(right.right, left.left))and (self.go(right.left, left.right))
+            
+        
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        else:
+            return self.go(root.right, root.left)
