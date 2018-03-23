3c597c704067e730a849f731b82029c7b31fb9c9
Vincentsean <34153546+Vincentsean@users.noreply.github.com>
2018-03-23 10:40:49 +0800

    Update binarytree-preodrer.py

--- binarytree-preodrer.py
+++ binarytree-preodrer.py
@@ -24,3 +24,22 @@ class Solution:
                 stack.append((root, True))
                 
         return result
+    
+    
+class Solution2:
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result, stack = [], []
+        while True:
+            while root:
+                result.append(root.val)
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return result
+            root = stack.pop().right
+
+        
